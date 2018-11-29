8db0fcd5a311e12ab4c361b3e55f3491dbc73b42
MiriamGaGuMuk <mgg_pjl@yahoo.com>
2018-11-28 22:31:31 -0600

    Fifth Task done

--- assignment-files/js/main.js
+++ assignment-files/js/main.js
@@ -4,41 +4,70 @@
 /* TASK 1 -- Show/Hide Nav */
 
 document.querySelector('#hide-nav button').addEventListener('click', function() {
-    document.querySelector('#hide-nav .nav-menu').classList.toggle('nav-menu-hidden');
+
+document.querySelector('#hide-nav .nav-menu').classList.toggle('nav-menu-hidden');
 
 
 /* TASK 2 -- Select/Deslect an Icon */
-let selector = document.querySelectorAll('#select-items .wish-list .option');
+let wishList = document.querySelectorAll('#select-items .wish-list .option');
+
 for (let i = 0; i < selector.length; i++) {
-  selector[i].addEventListener('click', function(evt) {
+
+    wishList[i].addEventListener('click', function(evt) {
+
     evt.currentTarget.classList.toggle('selected');
-  })
+
+  });
 }
 
 /* TASK 3 -- Increase total number*/
-let selector = document.querySelectorAll('#add-vals button');
+let totalpoints = document.querySelectorAll('#add-vals button');
+
 for (let i = 0; i < selector.length; i++) {
-  selector[i].addEventListener('click', function(evt) {
+
+    totalpoints[i].addEventListener('click', function(evt) {
+
     let increment = parseInt(evt.currentTarget.textContent);
+
     let current = parseInt(document.querySelector('#add-vals .total-points').textContent);
+
     document.querySelector('#add-vals .total-points').textContent = current + increment;
-  })
+  });
 }
 
 
 /* TASK 4 (Adventure Mode)-- Move Item List to List */
 let selector = document.querySelectorAll('.individual');
+
 for (let i = 0; i < selector.length; i++) {
+
   selector[i].addEventListener('click', function(evt) {
+
     let list = evt.currentTarget;
     let destiny = 'good-standing-list';
+
     if (list.parentNode.className === 'good-standing-list') {
       destiny = 'probation-list';
     }
+
     document.querySelector('ul.' + destiny).appendChild(list);
-  })
+  });
 }
 
 
 /* TASK 5 (Adventure Mode) -- Change Text Background Color From Palette */
+let palete = document.querySelectorAll(".palette span")
+    // console.log(fondoTexto);
+function changeBackground() {
 
+  let color = this.className
+    // console.log(color);
+  document.querySelector(".msg").className = "msg "+ color
+
+    }
+    for (let i = 0; i < palete.length; i++) {
+
+        let buttonPalete = palete[i]
+
+    buttonPalete.addEventListener('click', changeBackground)
+}
